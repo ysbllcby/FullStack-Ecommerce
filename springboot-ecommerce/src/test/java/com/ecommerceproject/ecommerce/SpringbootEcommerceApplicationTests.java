a98d76a76ba79ae40e8b73ad028633334ce56913
ysbllcby <ysabellacubay@gmail.com>
2023-06-24 20:34:56 -0600

    Initial commit.

---
+++ springboot-ecommerce/src/test/java/com/ecommerceproject/ecommerce/SpringbootEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerceproject.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
