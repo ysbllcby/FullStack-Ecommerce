b92d61711228c008b141139fa3ae0190a50d6cba
ysbllcby <ysabellacubay@gmail.com>
2023-06-28 22:34:57 -0600

    Added CrossOrigin to allow access from Angular app.

--- springboot-ecommerce/src/main/java/com/ecommerceproject/ecommerce/dao/ProductCategoryRepository.java
+++ springboot-ecommerce/src/main/java/com/ecommerceproject/ecommerce/dao/ProductCategoryRepository.java
@@ -3,7 +3,9 @@ package com.ecommerceproject.ecommerce.dao;
 import com.ecommerceproject.ecommerce.entity.ProductCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin("http://localhost:4200") // allow access from Angular app
 @RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
 }
--- springboot-ecommerce/src/main/java/com/ecommerceproject/ecommerce/dao/ProductRepository.java
+++ springboot-ecommerce/src/main/java/com/ecommerceproject/ecommerce/dao/ProductRepository.java
@@ -2,6 +2,8 @@ package com.ecommerceproject.ecommerce.dao;
 
 import com.ecommerceproject.ecommerce.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin("http://localhost:4200") // allow access from Angular app
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
